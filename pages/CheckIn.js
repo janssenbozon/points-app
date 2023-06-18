0c8c2b0ba8caa1a77af0eb5678ba01161a59af42
janssenbozon <janssenbozon@gmail.com>
2023-06-18 18:35:45 -0500

    Working on backend logic for check-in, issue where start and end times are the same. Might be sync issue

--- pages/CheckIn.js
+++ pages/CheckIn.js
@@ -2,54 +2,57 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { RecaptchaVerifier, signInWithPhoneNumber, onAuthStateChanged, getAuth } from "firebase/auth"
-import { authentication } from '../firebase/clientApp.ts'
-import React, { useState } from 'react';
-import { getDatabase, ref, set, get } from "firebase/database";
+import { authentication, database } from '../firebase/clientApp.ts'
+import React, { useState, useEffect } from 'react';
+import { getDatabase, ref, set, get, onValue } from "firebase/database";
 import { Router, useRouter } from 'next/router'
 
 export default function Login() {
 
-  const [id, setId] = useState("")
-  const [showConfirmation, setShowConfirmation] = useState(true)
+  const [showConfirmation, setShowConfirmation] = useState(false)
   const [showCodeScreen, setShowCodeScreen] = useState(true)
 
-
   // EVENT SPECIFIC DATA- LOADED AFTER CODE IS ENTERED
-  const [start, setStart] = useState("")
-  const [end, setEnd] = useState("")
+  const [start, setStart] = useState(new Date())
+  const [end, setEnd] = useState(new Date())
   const [name, setName] = useState("")
   const [category, setCategory] = useState("")
   const [points, setPoints] = useState(0)
 
-  function preCheckIn() {
+  function fetchEventData(id) {
+
+    console.log("Ref = events/" + id)
 
     const eventRef = ref(database, 'events/' + id);
 
-    eventRef.once('value')
-      .then((snapshot) => {
-        const eventData = snapshot.val();
-        setStart(new Date(eventData.startTime));
-        setEnd(new Date(eventData.endTime));
-        setName(eventData.eventName);
-        setCategory(eventData.eventCategory);
-        setPoints(eventData.eventPoints);
+    get(eventRef).then((snapshot) => {
+      const eventData = snapshot.val();
 
-        const currentDate = new Date();
-        const currentTime = currentDate.getTime();
+      console.log(eventData)
+      setStart(new Date(eventData.start));
+      setEnd(new Date(eventData.end));
+      setName(eventData.name);
+      setCategory(eventData.category);
+      setPoints(eventData.points);
+    });
+  }
 
-        if (currentTime >= eventStartTime.getTime() && currentTime <= eventEndTime.getTime()) {
-
-          // confirm with user
-          setShowConfirmation(true)
-
-        } else {
-          console.log('Not the correct time to check in.')
-        }
-      })
-      .catch((error) => {
-        console.error('Failed to fetch event data:', error);
-      });
-
+  function checkTime() {
+    const currentDate = new Date();
+    const startTime = start.getTime();
+    const endTime = end.getTime();
+    const currentTime = currentDate.getTime();
+  
+    if (currentTime >= startTime && currentTime <= endTime) {
+      console.log("Time correct!");
+      return true
+    } else {
+      console.log('Not the correct time to check in.');
+      console.log('currentTime = ' + currentTime);
+      console.log('startTime = ' + startTime);
+      console.log('endTime = ' + endTime);
+      return false
+    }
   }
 
   function checkIn() {
@@ -81,6 +84,9 @@ export default function Login() {
   }
 
   const CodeScreen = () => {
+
+    const [id, setId] = useState("")
+
     return (
         <>
           <h1 className="text-2xl font-bold font-lato text-center">
@@ -93,9 +99,9 @@ export default function Login() {
               <input
                 type="tel"
                 value={id}
-                className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-11 pr-12 sm:text-sm border-gray-300 rounded-xl transition ease-in-out"
+                className="focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-xl transition ease-in-out"
                 placeholder="123456"
-                onChange={e => setId(e.target.value)}
+                onChange={(e) => setId(e.target.value)}
               />
             </div>
 
@@ -104,8 +110,13 @@ export default function Login() {
                 type="submit"
                 className="inline-block px-6 py-2.5 bg-gray-800 text-white font-medium text-sm leading-tight uppercase rounded-lg shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out"
                 onClick={() => {
-                  setShowConfirmation(true);
-                  setShowCodeScreen(false);
+                  fetchEventData(id)
+                  if(checkTime()) {
+                    showCodeScreen(false)
+                    showConfirmation(true)
+                  } else {
+
+                  }
                 }}
               >Check In</button>
             </div>
@@ -122,9 +133,9 @@ export default function Login() {
         </h1>
 
         <h1 className="text-2xl text-lime-500 font-bold font-lato text-center">
-          Volleyball Tryouts
+          {name}
         </h1>
-        <h2 className='text-md font-bold font-lato text-center'>You gain 3 sports points from this.</h2>
+        <h2 className='text-md font-bold font-lato text-center'>You gain {points} {category} points from this.</h2>
         <div>
           <div className="flex space-x-2 justify-center pt-4">
             <button
