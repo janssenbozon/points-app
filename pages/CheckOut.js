1e9cebfef75442e79a25805b3b4b4b8248b96664
janssenbozon <57159519+janssenbozon@users.noreply.github.com>
2023-11-15 22:39:10 +0000

    Refined checkout process- now includes forfeit

--- pages/CheckOut.js
+++ pages/CheckOut.js
@@ -11,10 +11,15 @@ export default function Login() {
     const router = useRouter();
     const [event, setEvent] = useState(null);
     const [user, setUser] = useState(null);
-    const [currentPoints, setCurrentPoints] = useState(0);
-    const [showConfirmation, setShowConfirmation] = useState(true)
+    const [loading, setLoading] = useState(true);
+    const [showConfirmation, setShowConfirmation] = useState(false)
     const [showCompletion, setShowCompletion] = useState(false)
+    const [showForfeit, setShowForfeit] = useState(false)
+    const [showCodeScreen, setShowCodeScreen] = useState(false)
+    const [invalidCode, setInvalidCode] = useState(false)
     const [eventID, setEventID] = useState(null)
+    const uid = authentication.currentUser.uid;
+
     // Load event data from firebase before rendering
     useEffect(() => {
 
@@ -49,6 +54,8 @@ export default function Login() {
             });
 
         console.log("Event data loaded");
+        setLoading(false);
+        setShowCodeScreen(true);
 
     }, []);
 
@@ -121,6 +128,60 @@ export default function Login() {
         )
     }
 
+    const ForfeitScreen = () => {
+        return (
+            <>
+                <h1 className="text-2xl font-bold font-lato text-center">
+                    You&apos;re forfeiting your points from {event.name}.
+                </h1>
+                <h2 className='text-md font-bold font-lato text-center'>You&apos;ll lose {event.points} {event.category} points from this event.</h2>
+                <div>
+                    <div className="flex space-x-2 justify-center pt-4">
+
+                        { loading ? <div>Loading...</div> :
+                        <button
+                            type="submit"
+                            className='btn btn-error'
+                            onClick={() => {
+                                setShowForfeit(false);
+                                setLoading(true);
+
+                                const updates = {};
+                                updates['/users/' + uid + '/eventId'] = "NOT CHECKED IN";
+                                updates['/users/' + uid + '/eventName'] = "NOT CHECKED IN";
+                                updates['/users/' + uid + '/pastEvents/' + eventID] = {
+                                    name: event.name + " (FORFEITTED)",
+                                    start: event.start,
+                                    points: 0,
+                                    category: event.category
+                                };
+
+                                update(ref(database), updates).then(() => {
+                                    setShowCompletion(true);
+                                }).catch((error) => {
+                                    console.error('Failed to update user\'s status:', error);
+                                });
+
+                            }}
+                        >Forfeit and check out</button>
+                        }
+                    </div>
+                    <div className="flex space-x-2 justify-center pt-4">
+                        <button
+                            type="submit"
+                            className='btn'
+                            onClick={() => {
+                                setShowForfeit(false);
+                                setShowCodeScreen(true);
+                            }}
+                        >Back</button>
+                    </div>
+                    
+                </div>
+            </>
+        )
+    }
+
     // Completion out screen: alerts the user that the check out process is complete
     const CompletionScreen = () => {
         return (
@@ -143,20 +204,77 @@ export default function Login() {
         )
     }
 
+    const CodeScreen = () => {
+
+        const [id, setId] = useState("")
+
+        if (event === null) {
+            return <div>Loading...</div>; // Display loading message or placeholder
+        }
+
+    return (
+      <>
+        <h1 className="text-lg font-bold font-lato text-center">
+          Please enter the checkout code for:
+        </h1>
+        <h1 className="text-2xl text-green-400 font-bold font-lato text-center">{event.name}</h1>
+          {invalidCode ? <h3 className='text-md font-bold font-lato text-center text-red-500'>Invalid code.</h3> : null}
+          <div className="mt-1 relative rounded-md shadow-sm pt-3">
+            <div className="absolute inset-y-0 left-0 pl-3 pt-3 flex items-center pointer-events-none" />
+            <input
+              type="tel"
+              value={id}
+              className="input w-full"
+              placeholder="Enter code here"
+              onChange={(e) => setId(e.target.value)}
+            />
+          </div>
+
+          <div className="flex flex-col space-y-2 justify-center pt-4">
+            <button
+              type="submit"
+              className="btn btn-success"
+              onClick={() => {
+                console.log("Checking code")
+                if(id == event.endCode) {
+                    console.log("Code is correct")
+                    setShowCodeScreen(false)
+                    setShowConfirmation(true)
+                } else {
+                    setInvalidCode(true)
+                }
+              }}
+            >Check Out</button>
+            <button
+                className='btn btn-error'
+                type="submit"
+                onClick={() => {
+                    setShowCodeScreen(false)
+                    setShowForfeit(true)
+                }}
+            >I don't know the code!</button>
+            <button
+              className='btn'
+              type="submit"
+              onClick={() => router.push('/Homepage')}
+            >Return home</button>
+          </div>
+      </>
+    )
+
+    }
+
 
     return (
 
         <div className={styles.container}>
-
-            <Head>
-                <title>Create Next App</title>
-                <meta name="description" content="Generated by create next app" />
-                <link rel="icon" href="/favicon.ico" />
-                <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
-                <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet"></link>
-            </Head>
             <main className={styles.main}>
+
+                {loading ? <div>Loading...</div> : null}
+
+                {showCodeScreen ? <CodeScreen /> : null}
+
+                {showForfeit ? <ForfeitScreen /> : null}
 
                 {showConfirmation ? <ConfirmationScreen /> : null}
 
